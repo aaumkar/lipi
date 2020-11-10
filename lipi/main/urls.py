7b2ced28e24be191327f2d6198fbd31117b9550d
aumkar inamdar <auminam@live.com>
2020-11-10 14:13:12 +0530

    added 3 taal template

--- lipi/main/urls.py
+++ lipi/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . views import *
 
 urlpatterns = [
-
+    path('', show, name="index"),
+    path('teental/', teental, name='teental'),
 ]
--- lipi/main/views.py
+++ lipi/main/views.py
@@ -1,3 +1,12 @@
+from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def show(request):
+    return render(request, 'main/index.html', {})
+
+
+def teental(request):
+    return render(request, 'main/teental.html', {})
